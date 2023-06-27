ed1e8da974845fa016cc528e90efd6df8c15cb86
nealgoogs <thenextcr7@yahoo.com>
2023-06-26 21:49:39 -0400

    idk new visual stuff needs work

--- constants.py
+++ constants.py
@@ -11,3 +11,4 @@ RIOT_API_KEY = get_api_key()
 SUMMMONER_BY_NAME_URL = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key=" + RIOT_API_KEY
 MATCH_HISTORY_URL = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?start=0&count=20&api_key=" + RIOT_API_KEY
 MATCH_DETAILS_URL = "https://americas.api.riotgames.com/lol/match/v5/matches/{}?api_key=" + RIOT_API_KEY
+
